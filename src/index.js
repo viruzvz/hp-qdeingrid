3b62799bee836b26c3721d2c186bf938e3031201
viruzvz <viruzvz@gmail.com>
2021-09-21 14:59:09 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles/main.scss'
+import './styles/main.less'
+import 'popper.js'
+import 'bootstrap'
+import $ from 'jquery'
+window.$ = window.jQuery = $
+
+!(function($) {
+  "use strict"
+  
+
+})(jQuery)
